ce0e64f2b3c1c7cdc9094920d86ad1172d27ec1f
Misael Taveras <macrot29@gmail.com>
2019-07-22 09:44:40 -0400

    core: create empty pages for links

---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import GeneralLayout from '../layout/general'
+import { Typography } from '@material-ui/core'
+
+export default () => (
+  <GeneralLayout>
+    <Typography variant="subtitle1">Contact</Typography>
+  </GeneralLayout>
+)
