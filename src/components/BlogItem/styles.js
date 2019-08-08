4ec04f1087da8da0c25fa782ed86872a0eca0987
Misael Taveras <macrot29@gmail.com>
2019-08-08 00:53:40 -0400

    feat: add more space between blog items

--- src/components/BlogItem/styles.js
+++ src/components/BlogItem/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 export const useStyles = makeStyles(theme => ({
   root: {
     minWidth: 275,
+    marginBottom: theme.spacing(2),
   },
 
   excerpt: {
