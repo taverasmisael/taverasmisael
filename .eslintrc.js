c2a2d575367e4f7c89137d364e2b31921204ca91
Misael Taveras <macrot29@gmail.com>
2019-08-11 07:24:29 -0400

    feat: add prettier/eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: ['plugin:prettier/recommended', 'react-app'],
+  plugins: ['prettier'],
+  rules: {
+    indent: ['error', 2],
+  },
+}
