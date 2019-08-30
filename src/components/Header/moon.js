1264abb65650feda43b316b4d0906db39bd6cdeb
Misael Taveras <macrot29@gmail.com>
2019-08-30 09:05:09 -0400

    feat: change DarkToggle icons

---
+++ src/components/Header/moon.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import SvgIcon from '@material-ui/core/SvgIcon'
+
+const MoonIcon = props => (
+  <SvgIcon
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="transparent"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    {...props}
+  >
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+  </SvgIcon>
+)
+
+export default MoonIcon
