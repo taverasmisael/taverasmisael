69bcbd1064e9aa0a440735e8af778f7d65710f56
Misael Taveras <macrot29@gmail.com>
2019-09-01 09:01:27 -0400

    feat: remove main padding

--- src/layouts/general.js
+++ src/layouts/general.js
@@ -1,5 +1,4 @@
 import React, { memo } from 'react'
-import makeStyles from '@material-ui/core/styles/makeStyles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MDXProvider } from '@mdx-js/react'
 
@@ -10,30 +9,17 @@ import Head from '../components/Head'
 import Footer from '../components/Footer'
 import { DarkModeProvider } from '../context/dark-mode'
 
-const useStyles = makeStyles(theme => ({
-  content: {
-    padding: theme.spacing(0, 3, 2),
-
-    [theme.breakpoints.down('sm')]: {
-      padding: theme.spacing(0, 1),
-    },
-  },
-}))
-
-const GeneralLayout = memo(({ children, noGutterBottom, headProps }) => {
-  const classes = useStyles()
-  return (
-    <DarkModeProvider>
-      <CssBaseline />
-      <Head {...headProps} />
-      <Header noGutterBottom={noGutterBottom} />
-      <main className={classes.content}>
-        <MDXProvider components={MDXComponentsMap}>{children}</MDXProvider>
-      </main>
-      <Footer />
-    </DarkModeProvider>
-  )
-})
+const GeneralLayout = memo(({ children, noGutterBottom, headProps }) => (
+  <DarkModeProvider>
+    <CssBaseline />
+    <Head {...headProps} />
+    <Header noGutterBottom={noGutterBottom} />
+    <main>
+      <MDXProvider components={MDXComponentsMap}>{children}</MDXProvider>
+    </main>
+    <Footer />
+  </DarkModeProvider>
+))
 
 GeneralLayout.displayName = 'GeneralLayout'
 
