38033e4bf027893ed1f1f5387a9198e39c1e1118
Misael Taveras <macrot29@gmail.com>
2020-01-14 11:15:12 -0400

    feat: complete HeroIntro redesign for desktop

--- src/components/HeroIntro/HeroIntro.js
+++ src/components/HeroIntro/HeroIntro.js
@@ -1,29 +1,53 @@
 import React, { memo } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
+import webDesignerPictureSrc from '../../shared/assets/web-dev-working.svg'
 import { useStyles } from './styles'
+
+const CTA = withStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.secondary.dark,
+    color: theme.palette.secondary.contrastText,
+    fontSize: '1.5em',
+    marginTop: '3em',
+
+    '&:hover': {
+      // Secondary color with 5% less Light (HSL)
+      backgroundColor: '#8e0b30',
+    },
+  },
+}))(Button)
 
 const HeroIntro = memo(() => {
   const classes = useStyles()
   return (
     <section className={classes.root}>
       <div className={classes.content}>
-        <Typography
-          gutterBottom
-          variant="h2"
-          component="h1"
-          className={classes.title}
-        >
-          Hola, soy Misael Taveras
+        <Typography variant="h2" component="h1" className={classes.title}>
+          Hola
+          <Typography
+            variant="h2"
+            component="span"
+            className={classes.subtitle}
+          >
+            Soy <strong className={classes.name}>Misael Taveras</strong>
+          </Typography>
         </Typography>
         <Typography
           variant="subtitle1"
           component="h2"
-          className={classes.content}
+          className={classes.description}
         >
-          Soy un desarrollador FrontEnd que trabaja con React. Ayudo a otros
-          desarrolladores a avanzar en su carrera.
+          Yo creo Páginas y Aplicaciones Web.
         </Typography>
+        <CTA variant="contained" color="secondary" className={classes.cta}>
+          Get in touch
+        </CTA>
+      </div>
+      <div className={classes.image}>
+        <img src={webDesignerPictureSrc} alt="Desarrollador web trabajando" />
       </div>
     </section>
   )
--- src/components/HeroIntro/styles.js
+++ src/components/HeroIntro/styles.js
@@ -8,27 +8,56 @@ export const useStyles = makeStyles(theme => ({
         ? theme.palette.background.paper
         : theme.palette.primary.dark,
     color: theme.palette.primary.contrastText,
-    display: 'flex',
-    justifyContent: 'center',
-    textAlign: 'center',
-    minHeight: '35vh',
+    minHeight: '56vh',
     padding: theme.spacing(3, 2),
+    position: 'relative',
+  },
+  content: {
+    maxWidth: '40%',
+    margin: '0',
+    marginLeft: '8em',
+    marginTop: '8em',
+
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '100%',
+      marginLeft: 0,
+      marginTop: 0,
+    },
+  },
+
+  image: {
+    position: 'absolute',
+    right: '10%',
+    top: '25vh',
+    width: 640,
   },
 
   title: {
+    fontSize: '6.625rem',
     marginTop: 0,
+    marginBottom: 0,
+  },
+
+  subtitle: {
+    fontFamily: theme.typography.body1.fontFamily,
+    fontSize: '0.75em',
+    marginTop: '0!important',
+    marginBottom: 0,
+  },
+
+  name: {
+    color: theme.palette.secondary.main,
+  },
+
+  description: {
+    fontSize: '2.5rem',
+    fontWeight: 300,
   },
 
   contrastColor: {
     color: theme.palette.primary.contrastText,
   },
-
-  content: {
-    maxWidth: '60%',
-    margin: '0 auto',
-
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '100%',
-    },
+  cta: {
+    marginTop: '5em',
   },
 }))
