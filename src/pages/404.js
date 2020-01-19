1a67d4745dc5bcca05bd284adac1fd1d21053cec
Misael Taveras <macrot29@gmail.com>
2020-01-18 20:18:33 -0400

    feat: simplify and detatch layout from Home

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,6 @@ import { graphql, Link } from 'gatsby'
 
 import BlogItemMini from '../components/BlogItemMini'
 import GeneralLayout from '../layouts/general'
-import { useStyles } from '../components/HeroIntro/styles'
 
 const NotFoundPage = memo(({ data }) => {
   const entry = data.allMdx.nodes[0]
@@ -24,35 +23,32 @@ const NotFoundPage = memo(({ data }) => {
 })
 
 function Content({ entry }) {
-  const classes = useStyles()
   return (
     <>
-      <section className={classes.root}>
-        <div className={classes.content}>
-          <Typography component="h1" variant="h3" gutterBottom>
-            Lo que buscas no está aquí
-          </Typography>
-          <Typography variant="overline" component="h2" gutterBottom>
-            Es posible que la página se haya movido, no se haya creado o
-            simplemente no exista en esta dimensión.
-          </Typography>
-          <Typography>
-            Mientras, puedes{' '}
-            <MaterialLink component={Link} to="/" color="secondary">
-              volver al inicio
-            </MaterialLink>
-            ,{' '}
-            <MaterialLink component={Link} to="/blog" color="secondary">
-              visitar el blog
-            </MaterialLink>{' '}
-            o ponerte en{' '}
-            <MaterialLink component={Link} to="/contacto" color="secondary">
-              contacto conmigo
-            </MaterialLink>
-            .
-          </Typography>
-        </div>
-      </section>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h3">
+          Lo que buscas no está aquí
+        </Typography>
+        <Typography variant="overline" component="h2">
+          Es posible que la página se haya movido, no se haya creado o
+          simplemente no exista en esta dimensión.
+        </Typography>
+        <Typography>
+          Mientras, puedes{' '}
+          <MaterialLink component={Link} to="/" color="secondary">
+            volver al inicio
+          </MaterialLink>
+          ,{' '}
+          <MaterialLink component={Link} to="/blog" color="secondary">
+            visitar el blog
+          </MaterialLink>{' '}
+          o ponerte en{' '}
+          <MaterialLink component={Link} to="/contacto" color="secondary">
+            contacto conmigo
+          </MaterialLink>
+          .
+        </Typography>
+      </Container>
       <Container maxWidth="md">
         <Typography variant="h4" gutterBottom>
           Aquí está el ultimo post del blog
