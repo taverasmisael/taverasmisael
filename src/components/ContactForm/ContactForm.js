bd7046d2d95a0f016cc2fe8c1863a1af72835280
Misael Taveras <macrot29@gmail.com>
2020-02-06 11:54:29 -0400

    feat: complete new ContactForm layout

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { memo, useState } from 'react'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
 import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import Select from '@material-ui/core/Select'
@@ -7,7 +8,7 @@ import FilledInput from '@material-ui/core/FilledInput'
 import MenuItem from '@material-ui/core/MenuItem'
 import TextField from '@material-ui/core/TextField'
 
-const INPUT_PROPS = { disableUnderline: true }
+import { useStyles } from './styles'
 
 const ContactForm = () => {
   const [state, setState] = useState({
@@ -16,6 +17,7 @@ const ContactForm = () => {
     reason: 'Consult',
     message: '',
   })
+  const classes = useStyles()
 
   const handleChange = ({ target = {} }) => {
     setState(state => ({ ...state, [target.name]: target.value }))
@@ -28,6 +30,7 @@ const ContactForm = () => {
       netlify-honeypot="got-the-honey"
       method="POST"
       data-netlify="true"
+      className={classes.root}
     >
       <input type="hidden" name="form-name" value="contact" />
       <p className="hidden">
@@ -36,64 +39,66 @@ const ContactForm = () => {
           encontraste ;) ): <input name="got-the-honey" />
         </label>
       </p>
-      <TextField
-        fullWidth
-        required
-        InputProps={INPUT_PROPS}
-        onChange={handleChange}
-        value={state.name}
-        id="name"
-        name="name"
-        label="Nombre"
-        margin="normal"
-        variant="filled"
-      />
-      <TextField
-        fullWidth
-        required
-        InputProps={INPUT_PROPS}
-        onChange={handleChange}
-        value={state.email}
-        name="email"
-        type="email"
-        id="email"
-        label="E-mail"
-        margin="normal"
-        variant="filled"
-      />
-      <FormControl margin="normal" fullWidth variant="filled">
-        <InputLabel htmlFor="reason">Razón del mensaje</InputLabel>
-        <Select
-          required
-          disableUnderline
-          onChange={handleChange}
-          value={state.reason}
-          name="reason"
-          input={<FilledInput required name="reason" id="reason" />}
-        >
-          <MenuItem value="Inquire">Negocios</MenuItem>
-          <MenuItem value="Help">Ayuda / Pregunta</MenuItem>
-          <MenuItem value="Consult">Consulta</MenuItem>
-          <MenuItem value="Other">Otra</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField
-        fullWidth
-        required
-        InputProps={INPUT_PROPS}
-        multiline
-        onChange={handleChange}
-        value={state.message}
-        rows={15}
-        name="message"
-        id="message"
-        label="Mensaje"
-        margin="normal"
-        variant="filled"
-      />
-      <Button variant="contained" type="submit">
-        Enviar
-      </Button>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            required
+            onChange={handleChange}
+            value={state.name}
+            id="name"
+            name="name"
+            label="Nombre"
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            fullWidth
+            required
+            onChange={handleChange}
+            value={state.email}
+            name="email"
+            type="email"
+            id="email"
+            label="E-mail"
+            margin="normal"
+            variant="filled"
+          />
+          <FormControl margin="normal" fullWidth variant="filled">
+            <InputLabel htmlFor="reason">Razón del mensaje</InputLabel>
+            <Select
+              required
+              onChange={handleChange}
+              value={state.reason}
+              name="reason"
+              input={<FilledInput required name="reason" id="reason" />}
+            >
+              <MenuItem value="Inquire">Negocios</MenuItem>
+              <MenuItem value="Help">Ayuda / Pregunta</MenuItem>
+              <MenuItem value="Consult">Consulta</MenuItem>
+              <MenuItem value="Other">Otra</MenuItem>
+            </Select>
+          </FormControl>
+          <Button variant="contained" type="submit" className={classes.submit}>
+            Enviar
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            required
+            multiline
+            onChange={handleChange}
+            value={state.message}
+            rows={9}
+            name="message"
+            id="message"
+            label="Mensaje"
+            margin="normal"
+            variant="filled"
+          />
+        </Grid>
+      </Grid>
     </form>
   )
 }
--- src/components/ContactForm/styles.js
+++ src/components/ContactForm/styles.js
@@ -1,3 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export const useStyles = makeStyles(theme => ({}))
+export const useStyles = makeStyles(theme => ({
+  submit: {
+    marginTop: theme.spacing(2),
+  },
+}))
