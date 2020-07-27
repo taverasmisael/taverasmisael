c15dc3546454c627bf6b6f7bca843eee8244adf6
Misael Taveras <macrot29@gmail.com>
2020-07-27 11:20:36 -0400

    style: remove console.warns

--- src/components/mdx/Img.js
+++ src/components/mdx/Img.js
@@ -4,7 +4,6 @@ import makeStyles from '@material-ui/core/styles/makeStyles'
 
 const Img = memo(({ className, alt, ...props }) => {
   const classes = useStyles()
-  console.warn('props', props)
   return (
     <img className={classnames(classes.root, className)} alt={alt} {...props} />
   )
