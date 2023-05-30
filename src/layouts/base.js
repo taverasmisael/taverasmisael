70c600bc257e2b51336bdd092d19c385f2eec4d3
Misael Taveras <hello@taverasmisael.com>
2023-05-30 12:52:06 -0400

    core: update readme before archiving
    Make the readme a shared MD to use as a profile on GitHub.
    Move the old readme to docs/README.md

--- src/layouts/base.js
+++ src/layouts/base.js
@@ -7,6 +7,24 @@ const BaseLayout = memo(({ children, headProps }) => (
   <DarkModeProvider>
     <CssBaseline />
     <Head {...headProps} />
+    <p style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      padding: '1rem',
+      backgroundColor: '#f44336',
+      color: '#fff',
+      zIndex: 9999,
+      textAlign: 'center',
+      fontSize: '1.5rem',
+    }}>
+      Este sitio web está archivado. Visite el nuevo en
+      <a href="taverasmisael.com?from=oldsite" style={{ 
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+      }}>https://taverasmisael.com.</a>
+    </p>
     {children}
   </DarkModeProvider>
 ))
